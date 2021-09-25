69efbdb4a5f9e207c34196be0169d88f1a8b40c8
Taras Radevych <taras.radevych@gmail.com>
2021-09-25 23:31:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-let silver = 15.678;
-let gold = 90.2345;
-let platinum = 123.965;
-console.log("Максимальне число: "+Math.max(silver, gold, platinum));
-console.log("Мінімальне число: "+Math.min(silver, gold, platinum));
-summ = silver+gold+platinum;
+console.log("Зміни внесено");
+const silverPrice = 15.678;
+const goldPrice = 90.2345;
+const platinumPrice = 123.965;
+console.log("Максимальне число: "+Math.max(silverPrice, goldPrice, platinumPrice));
+console.log("Мінімальне число: "+Math.min(silverPrice, goldPrice, platinumPrice));
+summ = silverPrice + goldPrice + platinumPrice;
 console.log("Сума вартості всіх товарів: "+summ);
 
-let silverFloor = (Math.floor(silver))
-console.log("Ціле чиало вартості товару, округлене в менший бік: "+silverFloor);
+const silverPriceFloor = (Math.floor(silverPrice))
+console.log("Ціле чиало вартості товару, округлене в менший бік: "+silverPriceFloor);
 
-let goldFloor = (Math.floor(gold));
-console.log("Ціле чиало вартості товару, округлене в менший бік: "+goldFloor);
+const goldPriceFloor = (Math.floor(goldPrice));
+console.log("Ціле чиало вартості товару, округлене в менший бік: "+goldPriceFloor);
 
-let platinumFloor = (Math.floor(platinum));
-console.log("Ціле чиало вартості товару, округлене в менший бік: "+platinumFloor);
+const platinumPriceFloor = (Math.floor(platinumPrice));
+console.log("Ціле чиало вартості товару, округлене в менший бік: "+platinumPriceFloor);
 
-let summRounded = 300;
-summRound = silverFloor+goldFloor+platinumFloor;
+const summRounded = 300;
+summRound = silverPriceFloor+goldPriceFloor+platinumPriceFloor;
 console.log("Сума товарів, округлена до сотень "+Math.max(summRound,summRounded));//не зрозумів
 
-let floorSum = ((Math.floor(summ/100))*100);
-console.log("Сума заокруглена до меншого: "+floorSum);
+const floorSum = ((Math.floor(summ / 100)) * 100);
+console.log("Сума заокруглена до меншого: " +floorSum);
 
-function booleanSumm(floorSum) {
-	var x
-	if (floorSum % 2 == 0) {
-		x = true;
-	} else {
-		x = false;
-	}
-	return x;
+function checkIfEven(floorSum) {
+	return floorSum % 2 == 0
 
 }
 
-console.log("Число є парним: "+booleanSumm(floorSum));
+console.log("Число є парним: "+checkIfEven(floorSum));
 
 let pay = 500;
 console.log("Сума решти, при оплаті всіх товарів куп‘юрою 500 грн: "+(pay-summ));
 
 
-let price = [silver, gold, platinum];
+let price = [silverPrice, goldPrice, platinumPrice];
 let sum = 0;
 for( let i = 0; i < price.length; i++ ){
     sum += parseFloat( price[i], 10);
 }
 
-var avg = sum/price.length;
+const avg = sum/price.length;
 
 
 console.log("Середнє значення сум, округлене до другого знаку, після коми: "+avg.toFixed(2));
 
 
-let discount = Math.random()*101;
-let discountSumm = summ * discount/100;
+let discount = Math.random() * 101;
+let discountSumm = summ * discount / 100;
 let paySumm = summ - discountSumm;
 console.log("Сума до оплати з рондомною знижкою, заокругленою до 2 знаків після коми: "+paySumm.toFixed(2));
 
