ab6a70460250a782e5de97f126b5651657d79a80
Taras Radevych <taras.radevych@gmail.com>
2021-09-28 23:38:35 +0300

    hw-02

--- HW-02/script.js
+++ HW-02/script.js
@@ -1,1 +1,44 @@
+let n = prompt("Введіть ціле число 'n': ");
+nInteger = parseInt(n , 10);
+
+
+while(isNaN(nInteger)){
+    let n = prompt("Ви ввели не число, введіть коректне число ціле 'n': ");
+    nInteger = parseInt(n , 10);
+    console.log(nInteger);
+    
+}
+
+let m = prompt("Введіть ціле число 'm': ");
+mInteger = parseInt(m , 10);
+
+while(isNaN(mInteger) || nInteger > mInteger) {
+    let m = prompt("Ви ввели не число, введіть коректне число ціле 'm': ");
+    mInteger = parseInt(m , 10);
+    console.log(mInteger);
+    document.writeln(mInteger);
+}
+
+const flag = confirm("Пропускати парні числа?");
+
+let sum = 0;
+console.log(nInteger, mInteger);
+for (i = nInteger + 1; i<= mInteger; i++)
+{
+    if(flag)
+    {
+        if(i % 2 == 0)
+        {
+            continue;
+        }
+    }
+
+sum = sum + i;
+
+}
+console.log("Результат додавання чисел у діапазоні від 'n' до 'm': " + sum);
+document.writeln("Введене число 'n': " + nInteger);
+document.writeln("Введене число 'm': " + mInteger);
+document.writeln("Результат додавання чисел у діапазоні від 'n' до 'm': " + sum);
+
 
