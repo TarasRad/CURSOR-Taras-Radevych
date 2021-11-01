691c64da15be9383e04ed07cd3ac80c0b3aef698
Taras Radevych <91094523+TarasRad@users.noreply.github.com>
2021-11-01 19:19:08 +0200

    Add files via upload

--- HW-04/script.js
+++ HW-04/script.js
@@ -1,3 +1,4 @@
+//str.charAt(str.length - 1)
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
 const themes = [
@@ -11,15 +12,13 @@ function getPairs(students) {
   const men = [];
   const women = [];
   const groups = new Array(3);
+  for (let i = 0; i < students.length; i++) {
+    let name = students[i];
 
-  for (i in students) {
-    let name = students[i].split("");
-    let j = name.length - 1;
-
-    if (name[j] === "а") {
-      women.push(students[i]);
+    if (name.charAt(name.length - 1) === "а") {
+      women.push(name);
     } else {
-      men.push(students[i]);
+      men.push(name);
     }
   }
 
@@ -32,14 +31,14 @@ function getPairs(students) {
 
   return groups;
 }
-
+//groupsThemes[i][0] = groups[i].join(' і ');
 function getGroupThemes() {
   const groupsThemes = [];
   let groups = getPairs(students);
-  for (i in groups) {
+  for (let i in groups) {
     groupsThemes[i] = new Array(2);
 
-    groupsThemes[i][0] = groups[i];
+    groupsThemes[i][0] = groups[i].join(" і ");
     groupsThemes[i][1] = themes[i];
   }
   return groupsThemes;
@@ -47,7 +46,7 @@ function getGroupThemes() {
 
 function getStudentMarks() {
   const studentsMarks = [];
-  for (i in students) {
+  for (let i in students) {
     studentsMarks[i] = new Array(2);
 
     studentsMarks[i][0] = students[i];
@@ -59,18 +58,22 @@ function getStudentMarks() {
 function getStudentMarksTheme() {
   const groupsThemesMarks = [];
   let groupsThemes = getGroupThemes();
-  for (i in groupsThemes) {
+  for (let i in groupsThemes) {
     groupsThemesMarks[i] = new Array(2);
 
     groupsThemesMarks[i][0] = groupsThemes[i];
 
-    let rand = Math.random();
-    var mark = Math.floor(rand * 5) + 1;
-
-    groupsThemesMarks[i][1] = mark;
+    groupsThemesMarks[i][1] = getRandom();
   }
   return groupsThemesMarks;
 }
+function getRandom() {
+  let rand = Math.random();
+  var mark = Math.floor(rand * 5) + 1;
+
+  return mark;
+}
+
 console.log(getPairs(students));
 console.log(getGroupThemes());
 console.log(getStudentMarks());
